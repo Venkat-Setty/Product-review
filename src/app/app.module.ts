8fd719eab3b737dce52011772ace2b95f7c73950
venkat-setty <venkat.setty@cbre.com>
2018-10-25 10:08:45 +0700

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     RouterModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyApw21-ICztqAt8yw5uhkHcehjVzVjLK88',
+    authDomain: 'product-review-phani.firebaseapp.com',
+    databaseURL: 'https://product-review-phani.firebaseio.com',
+    projectId: 'product-review-phani',
+    storageBucket: 'product-review-phani.appspot.com',
+    messagingSenderId: '1067863481253',
+  },
 };
